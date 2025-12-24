9a9d25edce4b8845119429a1b56b4e13a630044d
Dr. Eric O. Flores <eric@florestoro.com>
2025-12-24 10:26:11 -0700

    Add header information and copyright notice
    New updates - Still working on image resizing problems.

--- rolocard.py
+++ rolocard.py
@@ -23,6 +23,15 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+"""
+By Dr. Eric O. Flores email: <eoftoro@gmail.com>
+Rolocard 3.2 — Document-Based Cards with Embedded Graphics
+
+MIT License
+
+Copyright (c) 2024 drericflores
+"""
+
 import sys
 import os
 import json
